e8ef55034423959e148846909ca9dd369008fbc0
O Wigley <obw.null@gmail.com>
2018-06-19 15:46:08 +0100

    0.0.4

---
+++ upgrade.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#
+# creates new release for lib
+# usage
+#   upgrade <new version> <input aar path>
+
+[ $# != 2  ]                            && echo "usage: upgrade <version> <input file>" && exit 1
+[ ! -d  "MELROSE_GROUP" ]               && echo "Run from the same directory, not from `pwd`..." && exit 1
+[ -d MELROSE_GROUP/soj-artifact/$1 ]    && echo "that revision already exists..." && ls -la MELROSE_GROUP/soj-artifact/$1  && exit 1
+[ ! -e $2 ]                             && echo "file cannot be found \"$2\" " && exit 1
+
+ mvn install:install-file -DgroupId=MELROSE_GROUP    \
+     -DartifactId=soj-artifact -Dversion="$1"        \
+     -Dfile="$2" -Dpackaging=aar -DgeneratePom=true  \
+     -DlocalRepositoryPath=.  -DcreateChecksum=true
+
+
+
+
+     
